5876b60f3ae39a27659954f960d9b8a97195ff3d
sulieman-kh <sul.h.kh@hotmail.com>
2023-01-20 16:40:50 +0300

    modifications to the layout

--- script.js
+++ script.js
@@ -2,17 +2,15 @@
 
 
 // Sticky navigation
-const navbarEl = document.querySelector(".navbar");
+const headerEl = document.querySelector(".header");
 // const listEl = document.querySelectorAll("a");
-const headerEl = document.querySelector("header");
-console.log(navbarEl);
-console.log(headerEl);
-// console.log(listEl);
+const imgHeaderEl = document.querySelector(".image__header");
+
 
 
 const obs = new IntersectionObserver((entries) => {
   const ent = entries[0];
-  console.log(ent);
+  // console.log(ent);
   if (!ent.isIntersecting) {
     document.body.classList.add("sticky");
 
@@ -25,8 +23,27 @@ const obs = new IntersectionObserver((entries) => {
   threshold: 0,
   rootMargin: "-50px"
 });
-obs.observe(headerEl)
+obs.observe(imgHeaderEl)
 
 
+const cardEl = document.querySelectorAll(".card");
+console.log(cardEl)
+const cardImgEl = document.querySelector(".card__img");
+const btnEL = document.querySelector(".card__button")
+document.addEventListener('DOMContentLoaded', cardActive);
 
+function cardActive(e) {
+  console.log(cardEl)
+  // cardEl.focus();
+
+  cardImgEl.classList.add('carda')
+  btnEL.classList.add('btna')
+
+  cardEl.forEach(function (e) {
+    e.addEventListener('mouseover', function () {
+      cardImgEl.classList.remove('carda')
+      btnEL.classList.remove('btna')
+    });
+  })
+}
 
